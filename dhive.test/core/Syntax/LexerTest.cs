61444fa591c85caac3839503560d00a96ad82ffd
Divyansh Singh <divy97@gmail.com>
2021-01-05 17:14:40 +0530

    Added tests for token pairs

--- dhive.test/core/Syntax/LexerTest.cs
+++ dhive.test/core/Syntax/LexerTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using dhive.core.Syntax;
 using Xunit;
 
@@ -16,16 +17,32 @@ namespace dhive.test.core.Syntax
             Assert.Equal(text, token.Text);
         }
 
+        [Theory]
+        [MemberData(nameof(GetTokensPairData))]
+        public void Lexer_Lex_TokenPairTest(SyntaxKind t1Kind, string t1Text, SyntaxKind t2Kind, string t2Text){
+            var text = t1Text + t2Text;
+            var tokens = SyntaxTree.ParseToken(text).ToArray();
+            Assert.Equal(2, tokens.Length);
+            Assert.Equal(tokens[0].Kind, t1Kind);
+            Assert.Equal(tokens[0].Text, t1Text);
+            Assert.Equal(tokens[1].Kind, t2Kind);
+            Assert.Equal(tokens[1].Text, t2Text);
+        }
+
         public static IEnumerable<Object[]> GetTokensData(){
-            foreach(var t in GetTokens()){
+            foreach(var t in GetTokens().Concat(GetSeparators())){
                 yield return new object[] {t.kind, t.text};
+            }
+        }
+
+         public static IEnumerable<Object[]> GetTokensPairData(){
+            foreach(var t in GetTokenPairs()){
+                yield return new object[] {t.t1Kind, t.t1Text, t.t2Kind, t.t2Text};
             }
         }
 
         private static IEnumerable<(SyntaxKind kind,string text)> GetTokens(){
             return new[]{
-
-                
                 (SyntaxKind.PlusToken, "+"),
                 (SyntaxKind.MinusToken, "-"),
                 (SyntaxKind.StarToken, "*"),
@@ -40,12 +57,6 @@ namespace dhive.test.core.Syntax
                 (SyntaxKind.EqualToken, "="),
                 (SyntaxKind.FalseKeyword, "false"),
                 (SyntaxKind.TrueKeyword, "true"),
-
-                (SyntaxKind.WhitespaceToken, " "),
-                (SyntaxKind.WhitespaceToken, "  "),
-                (SyntaxKind.WhitespaceToken, "\r"),
-                (SyntaxKind.WhitespaceToken, "\n"),
-                (SyntaxKind.WhitespaceToken, "\r\n"),
                 (SyntaxKind.IndentiferToken, "abc"),
                 (SyntaxKind.IndentiferToken, "a"),
                 (SyntaxKind.NumberToken, "1"),
@@ -54,5 +65,57 @@ namespace dhive.test.core.Syntax
             };
 
         }
-    }
+
+        private static IEnumerable<(SyntaxKind kind,string text)> GetSeparators(){
+            return new[]{
+                (SyntaxKind.WhitespaceToken, " "),
+                (SyntaxKind.WhitespaceToken, "  "),
+                (SyntaxKind.WhitespaceToken, "\r"),
+                (SyntaxKind.WhitespaceToken, "\n"),
+                (SyntaxKind.WhitespaceToken, "\r\n"),   
+            };
+
+        }
+
+        private static bool RequiresSeprator(SyntaxKind t1Kind, SyntaxKind t2Kind){
+            var t1isKeyword = t1Kind.ToString().EndsWith("Keyword");
+            var t2isKeyword = t2Kind.ToString().EndsWith("Keyword");
+
+            if(t1Kind == SyntaxKind.IndentiferToken && t2Kind == SyntaxKind.IndentiferToken){
+                return true;
+            }
+            if(t1isKeyword && t2isKeyword){
+                return true;
+            }
+            if((t1isKeyword && t2Kind == SyntaxKind.IndentiferToken) || (t2isKeyword && t1Kind == SyntaxKind.IndentiferToken)){
+                return true;
+            }
+            if(t1Kind == SyntaxKind.NumberToken && t2Kind == SyntaxKind.NumberToken){
+                return true;
+            }
+            if(t1Kind == SyntaxKind.ExclamationToken && t2Kind == SyntaxKind.EqualEqualToken){
+                return true;
+            }
+            if(t1Kind == SyntaxKind.ExclamationToken && t2Kind == SyntaxKind.EqualToken){
+                return true;
+            }
+            if(t1Kind == SyntaxKind.EqualToken && t2Kind == SyntaxKind.EqualEqualToken){
+                return true;
+            }
+            if(t1Kind == SyntaxKind.EqualToken && t2Kind == SyntaxKind.EqualToken){
+                return true;
+            }
+            return false;
+        }
+
+         private static IEnumerable<(SyntaxKind t1Kind, string t1Text, SyntaxKind t2Kind, string t2Text)> GetTokenPairs(){
+            foreach(var t1 in GetTokens()){
+                foreach(var t2 in GetTokens()){
+                    if(RequiresSeprator(t1.kind, t2.kind)) continue;
+                    yield return (t1.kind, t1.text, t2.kind, t2.text);
+                }
+            }    
+        }
+    }  
+        
 }
