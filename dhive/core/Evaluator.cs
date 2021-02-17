2d4404e43b7d6c37ce4db37faf5eb86971951e47
divyans9711 <divy97@gmail.com>
2021-02-17 22:06:15 +0530

    Replaced is check with switch statements

--- dhive/core/Evaluator.cs
+++ dhive/core/Evaluator.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using dhive.core.Binding;
-using dhive.core.Syntax;
 using dhive.core;
 namespace core{
     internal sealed  class Evaluator{
@@ -16,58 +15,79 @@ namespace core{
         public object Evaluate(){
             return EvaluateExpression(_root);
         }
-        private object EvaluateExpression(BoundExpression root){
-            if(root is BoundLiteralExpression n)
-                return n.Value;
-            if(root is BoundVariableExpression v)
-                return _variables[v.Variable];
-            if(root is BoundAssignmentExpression a){
-                var value = EvaluateExpression(a.Expression);
-                _variables[a.Variable] = value;
-                return value;
+        private object EvaluateExpression(BoundExpression root)
+        {
+            switch (root.Kind)
+            {
+                case BoundNodeKind.LiteralExpression:
+                    return EvaluateLiteral((BoundLiteralExpression)root);
+                case BoundNodeKind.VariableExpression:
+                    return EvaluateVariableExpression((BoundVariableExpression)root);
+                case BoundNodeKind.AssignmentExpression:
+                    return EvaluateAssignementExpression((BoundAssignmentExpression)root);
+                case BoundNodeKind.BinaryExpression:
+                    return EvaluateBinaryExpression((BoundBinaryExpression)root);
+                case BoundNodeKind.UnaryExpression:
+                    return EvaulateUnaryExpression((BoundUnaryExpression)root);
+                default:
+                    throw new Exception($"EXC: Unexpected operator {root.Kind}");
             }
-            if(root is BoundBinaryExpression b){
-                var left = EvaluateExpression(b.Left);
-                var right = EvaluateExpression(b.Right);
-                switch(b.Op.Kind){
-                    case BoundBinaryOperatorKind.Addition:
-                        return (int) left + (int) right;
-                    case BoundBinaryOperatorKind.Substraction:
-                        return (int) left - (int) right;
-                    case BoundBinaryOperatorKind.Multiplication:
-                        return (int) left * (int) right;
-                    case BoundBinaryOperatorKind.Division:
-                        return (int) left/(int) right;
-                    case BoundBinaryOperatorKind.LogicalAnd:
-                        return (bool) left && (bool) right;
-                    case BoundBinaryOperatorKind.LogicalOr:
-                        return (bool) left || (bool) right;
-                    case BoundBinaryOperatorKind.Equals:
-                        return Equals(left, right);
-                    case BoundBinaryOperatorKind.NotEquals:
-                        return !Equals(left, right);
-                    default:
-                        throw new Exception($"EXC: Invalid operator {b.Op.Kind}");
-                }
+        }
+        private object EvaluateBinaryExpression(BoundBinaryExpression b)
+        {
+            var left = EvaluateExpression(b.Left);
+            var right = EvaluateExpression(b.Right);
+            switch (b.Op.Kind)
+            {
+                case BoundBinaryOperatorKind.Addition:
+                    return (int)left + (int)right;
+                case BoundBinaryOperatorKind.Substraction:
+                    return (int)left - (int)right;
+                case BoundBinaryOperatorKind.Multiplication:
+                    return (int)left * (int)right;
+                case BoundBinaryOperatorKind.Division:
+                    return (int)left / (int)right;
+                case BoundBinaryOperatorKind.LogicalAnd:
+                    return (bool)left && (bool)right;
+                case BoundBinaryOperatorKind.LogicalOr:
+                    return (bool)left || (bool)right;
+                case BoundBinaryOperatorKind.Equals:
+                    return Equals(left, right);
+                case BoundBinaryOperatorKind.NotEquals:
+                    return !Equals(left, right);
+                default:
+                    throw new Exception($"EXC: Invalid operator {b.Op.Kind}");
             }
-            if(root is BoundUnaryExpression u){
-                var operand = EvaluateExpression(u.Operand);
-                switch(u.Op.Kind){
-                    case BoundUnaryOperatorKind.Identity:
-                        return (int) operand;
-                    case BoundUnaryOperatorKind.Negation:
-                        return - (int) operand;
-                    case BoundUnaryOperatorKind.LogicalNegation:
-                        return ! (bool) operand;
-                    default:
-                        throw new Exception($"EXC: Invalid operator {u.Op.Kind}");
+        }
+        private object EvaulateUnaryExpression(BoundUnaryExpression u)
+        {
+            var operand = EvaluateExpression(u.Operand);
+            switch (u.Op.Kind)
+            {
+                case BoundUnaryOperatorKind.Identity:
+                    return (int)operand;
+                case BoundUnaryOperatorKind.Negation:
+                    return -(int)operand;
+                case BoundUnaryOperatorKind.LogicalNegation:
+                    return !(bool)operand;
+                default:
+                    throw new Exception($"EXC: Invalid operator {u.Op.Kind}");
 
-                }
             }
-            // if (root is ParenthesizedExpressionSyntax p){
-            //     return EvaluateExpression(p.Expression);
-            // }
-            throw new Exception($"EXC: Invalid operator {root.Kind}");
+        }
+        private object EvaluateAssignementExpression(BoundAssignmentExpression a)
+        {
+            var value = EvaluateExpression(a.Expression);
+            _variables[a.Variable] = value;
+            return value;
+        }
+        private object EvaluateVariableExpression(BoundVariableExpression v)
+        {
+            return _variables[v.Variable];
+        }
+        private static object EvaluateLiteral(BoundLiteralExpression n)
+        {
+            return n.Value;
         }
     }
     
