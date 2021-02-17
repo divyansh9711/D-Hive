b99f46148279c92f9324411f11f2c8c0ca2ce248
divyans9711 <divy97@gmail.com>
2021-02-18 00:22:09 +0530

    Rearranged properties of BinaryExpressionSyntax

--- dhive/core/Syntax/BinaryExpressionSyntax.cs
+++ dhive/core/Syntax/BinaryExpressionSyntax.cs
@@ -4,14 +4,15 @@ namespace dhive.core.Syntax
 {
     public sealed class BinaryExpressionSyntax: ExpressionSyntax{
         public BinaryExpressionSyntax(ExpressionSyntax left, SyntaxToken operatorToken, ExpressionSyntax right){
-            Left = left;    
+            Left = left;  
+            OperatorToken = operatorToken;  
             Right = right;
-            OperatorToken = operatorToken;
         }
         public override SyntaxKind Kind => SyntaxKind.BinaryExpression;
        
         public ExpressionSyntax Left {get; }
-        public ExpressionSyntax Right {get; }
         public SyntaxToken OperatorToken {get ;}
+        public ExpressionSyntax Right {get; }
+        
     }
 }
