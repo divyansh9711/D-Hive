b874a874a6b2b9bc7423f2d7dc5dc5450eacf947
divyans9711 <divy97@gmail.com>
2021-02-21 21:38:41 +0530

    Moved console print method to SyntaxNode

--- dhive/core/Syntax/SyntaxNode.cs
+++ dhive/core/Syntax/SyntaxNode.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 
@@ -32,6 +34,36 @@ namespace dhive.core.Syntax{
                     foreach (var child in children)                    
                         yield return child;
                 }
+            }
+        }
+
+        public void WriteTo(TextWriter writer){
+            PrettyPrint(writer, this);
+        }
+
+        static void PrettyPrint(TextWriter writer, SyntaxNode node, String indent = "", bool isLast = true){
+            var marker = isLast ? "└──" : "├──";
+            writer.Write(indent);
+            writer.Write(marker);
+            writer.Write(node.Kind);
+            if (node is SyntaxToken t && t.Value != null){
+                writer.Write(" ");
+                writer.Write(t.Value);
+            }
+            writer.WriteLine();
+            indent += isLast ? "   " : "│   ";
+            var lastChild = node.GetChildren().LastOrDefault();
+            indent += "    ";
+            foreach (var child in node.GetChildren()){
+                PrettyPrint(writer, child, indent, child == lastChild);
+            }
+        }
+
+        public override string ToString()
+        {
+            using (var writer = new StringWriter()){
+                WriteTo(writer);
+                return writer.ToString();
             }
         }
     }
--- driver/Program.cs
+++ driver/Program.cs
@@ -11,17 +11,17 @@ namespace core
         static void Main(){
             var showTree = false;
             var variables = new Dictionary<VariableSymbol, object>();
-            while (true)
-            {
+            while (true){
                 Console.Write(">");
                 var line = Console.ReadLine();
-                if(string.IsNullOrWhiteSpace(line))
+                if (string.IsNullOrWhiteSpace(line))
                     return;
-                if(line == "/showtree"){
+                if (line == "$showtree") {
                     showTree = !showTree;
-                    Console.WriteLine(showTree ? "Showing parse tree": "Not Showing ParseTree");
+                    Console.WriteLine(showTree ? "Showing parse tree" : "Not Showing ParseTree");
                     continue;
-                }else if(line == "/cls"){
+                }
+                else if (line == "$cls"){
                     Console.Clear();
                     continue;
                 }
@@ -29,13 +29,15 @@ namespace core
                 var compiler = new Compiler(syntaxTree);
                 var result = compiler.Evaluate(variables);
                 var diagnostics = result.Diagnostics;
-                if(showTree){
+                if (showTree) {
                     Console.ForegroundColor = ConsoleColor.DarkGray;
-                    PrettyPrint(syntaxTree.Root);
+                    syntaxTree.Root.WriteTo(Console.Out);
                     Console.ResetColor();
                 }
-                if (diagnostics.Any()){
-                    foreach (var diagnostic in diagnostics){
+                if (diagnostics.Any())
+                {
+                    foreach (var diagnostic in diagnostics)
+                    {
                         //TODO: Handle this case "1 +" 
                         Console.WriteLine();
                         Console.ForegroundColor = ConsoleColor.DarkRed;
@@ -53,26 +55,10 @@ namespace core
                         Console.WriteLine();
                     }
                     Console.ResetColor();
-                }else{
+                }
+                else{
                     Console.WriteLine(result.Value);
                 }
-            }
-        }
-        static void PrettyPrint(SyntaxNode node, String indent = "", bool isLast = true){
-            var marker = isLast ? "└──" : "├──";
-            Console.Write(indent);
-            Console.Write(marker);
-            Console.Write(node.Kind);
-            if (node is SyntaxToken t && t.Value != null){
-                Console.Write(" ");
-                Console.Write(t.Value);
-            }
-            Console.WriteLine();
-            indent += isLast ? "   " : "│   ";
-            var lastChild = node.GetChildren().LastOrDefault();
-            indent += "    ";
-            foreach (var child in node.GetChildren()){
-                PrettyPrint(child, indent, child == lastChild);
             }
         }
     }
